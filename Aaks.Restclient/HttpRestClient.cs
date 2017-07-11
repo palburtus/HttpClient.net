b74087f268a7c8666d9c33231f696de99dcef379
palburtus <patrick.alburtus@gmail.com>
2017-07-10 21:32:36 -0400

    added POST async

--- Aaks.Restclient/HttpRestClient.cs
+++ Aaks.Restclient/HttpRestClient.cs
@@ -27,6 +27,70 @@ namespace Aaks.Restclient
             return await GetAsync<T>(url, null);
         }
 
+        public async Task<HttpResponse<T>> PostAsync<T,K>(string url, Dictionary<string, string> headers, K body)
+        {
+            try
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+                if (headers != null)
+                {
+                    foreach (string key in headers.Keys)
+                    {
+                        httpWebRequest.Headers.Add(key, headers[key]);
+                    }
+                }
+
+                var serializedResult = Serialize<K>(body);
+                byte[] requestBody = Encoding.UTF8.GetBytes(serializedResult);
+
+                using (var postStream = await httpWebRequest.GetRequestStreamAsync())
+                {
+                    await postStream.WriteAsync(requestBody, 0, requestBody.Length);
+                }
+
+                var response = (HttpWebResponse)await httpWebRequest.GetResponseAsync();
+                
+                var reader = new StreamReader(response.GetResponseStream());
+                // ASYNC: using StreamReader's async method to read to end, in case
+                // the stream i slarge.
+                string stream = await reader.ReadToEndAsync();
+                HttpResponse<T> httpResposne = new HttpResponse<T>();
+                Type type = typeof(T);
+                if (type != typeof(string))
+                {
+                    httpResposne.Body = Deserialize<T>(stream);
+                }
+                else
+                {
+                    httpResposne.Body = (T)Convert.ChangeType(stream, typeof(T));
+                }
+                httpResposne.StatusCode = HttpStatusCode.OK;
+                return httpResposne;
+                
+
+            }
+            catch (WebException e)
+            {
+                using (WebResponse webResponse = e.Response)
+                {
+                    HttpWebResponse httpResponse = (HttpWebResponse)webResponse;
+                    Console.WriteLine("Error code: {0}", httpResponse.StatusCode);
+
+                    using (Stream data = webResponse.GetResponseStream())
+                    using (var reader = new StreamReader(data))
+                    {
+                        HttpResponse<T> response = new HttpResponse<T>();
+                        response.ErrorMessage = reader.ReadToEnd();
+                        response.StatusCode = httpResponse.StatusCode;
+                        return response;
+
+                    }
+                }
+            }
+        }
+
         public HttpResponse<T> Get<T>(string url, Dictionary<string, string> headers)
         {
             try
@@ -41,6 +105,7 @@ namespace Aaks.Restclient
                         httpWebRequest.Headers.Add(key, headers[key]);
                     }
                 }
+
                
                 HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
 
@@ -139,7 +204,7 @@ namespace Aaks.Restclient
             }
         }
 
-        private static string Serialize<T>(T obj)
+        public static string Serialize<T>(T obj)
         {
             DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
             MemoryStream ms = new MemoryStream();
@@ -148,7 +213,7 @@ namespace Aaks.Restclient
             return retVal;
         }
 
-        private static T Deserialize<T>(string json)
+        public static T Deserialize<T>(string json)
         {
             T obj = Activator.CreateInstance<T>();
             MemoryStream ms = new MemoryStream(Encoding.Unicode.GetBytes(json));
